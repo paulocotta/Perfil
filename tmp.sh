fee505059f4bacf51ecb0a0ad7e4cdef958c131d
Paulo Cotta <cotta.paulo@gmail.com>
2023-02-08 17:21:32 -0300

    Create tmp.sh

---
+++ tmp.sh
@@ -0,0 +1,10 @@
+Python é uma linguagem de programação de alto nível e interpretada, projetada para ser fácil de ler e escrever. É uma das linguagens mais populares na comunidade de desenvolvimento de software devido a sua versatilidade, facilidade de uso e ampla gama de aplicações. Desde a análise de dados, inteligência artificial, ciência da computação até o desenvolvimento de aplicações web, Python é uma linguagem amplamente utilizada e muito valorizada.
+
+Já o Selenium é uma ferramenta de automação de teste para aplicativos web. Ele permite aos desenvolvedores automatizarem ações de navegação e interação com a página web, como clicar em links, preencher formulários e verificar o conteúdo da página. O Selenium é compatível com vários navegadores, incluindo o Google Chrome, Mozilla Firefox e Internet Explorer, o que o torna uma ferramenta muito versátil. Além disso, ele permite aos desenvolvedores escrever scripts em várias linguagens de programação, incluindo Python.
+
+Quando se trata de recursos do sistema operacional, o shell script é uma forma de automatizar tarefas no sistema operacional, geralmente através da linha de comando. É uma sequência de comandos escritos em uma linguagem de script específica, como o Bash, que são executados pelo shell do sistema operacional. Com shell scripts, os administradores de sistemas podem automatizar tarefas repetitivas, como backup de arquivos, monitoramento de serviços e geração de relatórios, entre outras coisas. Eles também são úteis para criar scripts personalizados para automatizar tarefas específicas.
+
+Em resumo, Python é uma linguagem de programação versátil e fácil de usar, o Selenium é uma ferramenta de automação de teste para aplicativos web e o shell script é uma forma de automatizar tarefas no sistema operacional. Juntos, eles são uma combinação poderosa para a automação de tarefas e o desenvolvimento de aplicações.
+
+
+
